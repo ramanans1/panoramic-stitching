221076a2efbfc0f70945d3ec5fd2f198e579a8b0
ramanans1 <ramanans@seas.upenn.edu>
2019-03-03 19:40:24 -0500

    modified wrapper, readme

--- wrapper.py
+++ wrapper.py
@@ -10,70 +10,72 @@ from ransac_est_homography import *
 from mymosaic import *
 
 
+def wrapper():
+    #SET 1
+    im1 = np.array(Image.open('SET1_L.jpg').convert('RGB'))
+    im2 = np.array(Image.open('SET1_M.jpg').convert('RGB'))
+    im3 = np.array(Image.open('SET1_R.jpg').convert('RGB'))
 
-#SET 1
-im1 = np.array(Image.open('SET1_L.jpg').convert('RGB'))
-im2 = np.array(Image.open('SET1_M.jpg').convert('RGB'))
-im3 = np.array(Image.open('SET1_R.jpg').convert('RGB'))
+    #DEFINING THE INPUT IMAGE MATRIX
 
-#DEFINING THE INPUT IMAGE MATRIX
+    img_input = np.zeros((3,),dtype='object')
+    img_input[0] = im1
+    img_input[1]= im2
+    img_input[2]= im3
 
-img_input = np.zeros((3,),dtype='object')
-img_input[0] = im1
-img_input[1]= im2
-img_input[2]= im3
-
-#GETTING THE FINAL MOSAIC
-final_mosaic = mymosaic(img_input)
-
-
-#PLOTTING THE FINAL MOSAIC
-plt.imshow(final_mosaic)
-plt.show()
+    #GETTING THE FINAL MOSAIC
+    final_mosaic = mymosaic(img_input)
 
 
-
-'''
-##############################################################################
-#SET 2
-im1 = np.array(Image.open('SET2_L.jpg').convert('RGB'))
-im2 = np.array(Image.open('SET2_M.jpg').convert('RGB'))
-im3 = np.array(Image.open('SET2_R.jpg').convert('RGB'))
-
-#DEFINING THE INPUT IMAGE MATRIX
-
-img_input = np.zeros((3,),dtype='object')
-img_input[0] = im1
-img_input[1]= im2
-img_input[2]= im3
-
-#GETTING THE FINAL MOSAIC
-final_mosaic = mymosaic(img_input)
+    #PLOTTING THE FINAL MOSAIC
+    plt.imshow(final_mosaic)
+    plt.show()
 
 
-#PLOTTING THE FINAL MOSAIC
-plt.imshow(final_mosaic)
-plt.show()
-##############################################################################
-'''
 
-'''
-##############################################################################
-#TEST IMAGE
-im1 = np.array(Image.open('1_M.jpg').convert('RGB'))
-im2 = np.array(Image.open('1_R.jpg').convert('RGB'))
+    '''
+    ##############################################################################
+    #SET 2
+    im1 = np.array(Image.open('SET2_L.jpg').convert('RGB'))
+    im2 = np.array(Image.open('SET2_M.jpg').convert('RGB'))
+    im3 = np.array(Image.open('SET2_R.jpg').convert('RGB'))
 
-#TEST IMAGE INPUT MATRIX
-img_input = np.zeros((2,),dtype='object')
-img_input[0] = im1
-img_input[1]= im2
+    #DEFINING THE INPUT IMAGE MATRIX
 
-#GETTING THE FINAL MOSAIC
-final_mosaic = mymosaic(img_input)
+    img_input = np.zeros((3,),dtype='object')
+    img_input[0] = im1
+    img_input[1]= im2
+    img_input[2]= im3
+
+    #GETTING THE FINAL MOSAIC
+    final_mosaic = mymosaic(img_input)
 
 
-#PLOTTING THE FINAL MOSAIC
-plt.imshow(final_mosaic)
-plt.show()
-##############################################################################
-'''
+    #PLOTTING THE FINAL MOSAIC
+    plt.imshow(final_mosaic)
+    plt.show()
+    ##############################################################################
+    '''
+
+    '''
+    ##############################################################################
+    #TEST IMAGE
+    im1 = np.array(Image.open('1_M.jpg').convert('RGB'))
+    im2 = np.array(Image.open('1_R.jpg').convert('RGB'))
+
+    #TEST IMAGE INPUT MATRIX
+    img_input = np.zeros((2,),dtype='object')
+    img_input[0] = im1
+    img_input[1]= im2
+
+    #GETTING THE FINAL MOSAIC
+    final_mosaic = mymosaic(img_input)
+
+
+    #PLOTTING THE FINAL MOSAIC
+    plt.imshow(final_mosaic)
+    plt.show()
+    ##############################################################################
+    '''
+if __name__=="__main__":
+    wrapper()
